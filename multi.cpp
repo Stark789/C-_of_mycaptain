fe7e06052b5bf16856a29fb2e533e663d7ca42b6
Stark789 <85982161+Stark789@users.noreply.github.com>
2021-12-01 19:24:10 +0530

    Add files via upload

---
+++ multi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+  double num1, num2, product;
+  cout << "Enter two numbers: "<<endl;
+  cin >> num1 >> num2;
+  cout<<num1*num2<<endl;
+}
